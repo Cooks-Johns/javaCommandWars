85bd238cdfa0dcd4cb38c5780a688151a7fc56be
John <cooks.johns@gmail.com>
2018-05-28 21:16:49 -0500

    refactoring class's

--- src/PlayerList.java
+++ src/PlayerList.java
@@ -18,143 +18,143 @@ public class PlayerList {
 
     // make a method with math,randon math.floor to make a random roller
     // also think about the max luck each character gets
-
-
-    public static String getPlayerName() {
-        return playerName;
-    }
-
-    public static void setPlayerName(String playerName) {
-        PlayerList.playerName = playerName;
-    }
-
-    public static String getCharclass() {
-        return charclass;
-    }
-
-    public static void setCharclass(String charclass) {
-        PlayerList.charclass = charclass;
-    }
-
-    public static int getHp() {
-        return hp;
-    }
-
-    public static void setHp(int hp) {
-        PlayerList.hp = hp;
-    }
-
-    public static int getMaxHp() {
-        return maxHp;
-    }
-
-    public static void setMaxHp(int maxHp) {
-        PlayerList.maxHp = maxHp;
-    }
-
-    public static double getArmor() {
-        return armor;
-    }
-
-    public static void setArmor(double armor) {
-        PlayerList.armor = armor;
-    }
-
-    public static double getMaxArmor() {
-        return maxArmor;
-    }
-
-    public static void setMaxArmor(double maxArmor) {
-        PlayerList.maxArmor = maxArmor;
-    }
-
-    public static int getMana() {
-        return mana;
-    }
-
-    public static void setMana(int mana) {
-        PlayerList.mana = mana;
-    }
-
-    public static int getMaxMana() {
-        return maxMana;
-    }
-
-    public static void setMaxMana(int maxMana) {
-        PlayerList.maxMana = maxMana;
-    }
-
-    public static int getMeleeDmg() {
-        return meleeDmg;
-    }
-
-    public static void setMeleeDmg(int meleeDmg) {
-        PlayerList.meleeDmg = meleeDmg;
-    }
-
-    public static int getMaxMeleeDmg() {
-        return maxMeleeDmg;
-    }
-
-    public static void setMaxMeleeDmg(int maxMeleeDmg) {
-        PlayerList.maxMeleeDmg = maxMeleeDmg;
-    }
-
-    public static int getSpeed() {
-        return speed;
-    }
-
-    public static void setSpeed(int speed) {
-        PlayerList.speed = speed;
-    }
-
-    public static int getMaxSpeed() {
-        return maxSpeed;
-    }
-
-    public static void setMaxSpeed(int maxSpeed) {
-        PlayerList.maxSpeed = maxSpeed;
-    }
-
-    public static int getLuck() {
-        return luck;
-    }
-
-    public static void setLuck(int luck) {
-        PlayerList.luck = luck;
-    }
-
-    public static int getMaxLuck() {
-        return maxLuck;
-    }
-
-    public static void setMaxLuck(int maxLuck) {
-        PlayerList.maxLuck = maxLuck;
-    }
-
-    public static int getClassAbility() {
-        return classAbility;
-    }
-
-    public static void setClassAbility(int classAbility) {
-        PlayerList.classAbility = classAbility;
-    }
-
-    public static int getLevel() {
-        return level;
-    }
-
-    public static void setLevel(int level) {
-        PlayerList.level = level;
-    }
-
-    public static int getXp() {
-        return xp;
-    }
-
-    public static void setXp(int xp) {
-        PlayerList.xp = xp;
-    }
+//
+//
+//    public static String getPlayerName() {
+//        return playerName;
+//    }
+//
+//    public static void setPlayerName(String playerName) {
+//        PlayerList.playerName = playerName;
+//    }
+//
+//    public static String getCharclass() {
+//        return charclass;
+//    }
+//
+//    public static void setCharclass(String charclass) {
+//        PlayerList.charclass = charclass;
+//    }
+//
+//    public static int getHp() {
+//        return hp;
+//    }
+//
+//    public static void setHp(int hp) {
+//        PlayerList.hp = hp;
+//    }
+//
+//    public static int getMaxHp() {
+//        return maxHp;
+//    }
+//
+//    public static void setMaxHp(int maxHp) {
+//        PlayerList.maxHp = maxHp;
+//    }
+//
+//    public static double getArmor() {
+//        return armor;
+//    }
+//
+//    public static void setArmor(double armor) {
+//        PlayerList.armor = armor;
+//    }
+//
+//    public static double getMaxArmor() {
+//        return maxArmor;
+//    }
+//
+//    public static void setMaxArmor(double maxArmor) {
+//        PlayerList.maxArmor = maxArmor;
+//    }
+//
+//    public static int getMana() {
+//        return mana;
+//    }
+//
+//    public static void setMana(int mana) {
+//        PlayerList.mana = mana;
+//    }
+//
+//    public static int getMaxMana() {
+//        return maxMana;
+//    }
+//
+//    public static void setMaxMana(int maxMana) {
+//        PlayerList.maxMana = maxMana;
+//    }
+//
+//    public static int getMeleeDmg() {
+//        return meleeDmg;
+//    }
+//
+//    public static void setMeleeDmg(int meleeDmg) {
+//        PlayerList.meleeDmg = meleeDmg;
+//    }
+//
+//    public static int getMaxMeleeDmg() {
+//        return maxMeleeDmg;
+//    }
+//
+//    public static void setMaxMeleeDmg(int maxMeleeDmg) {
+//        PlayerList.maxMeleeDmg = maxMeleeDmg;
+//    }
+//
+//    public static int getSpeed() {
+//        return speed;
+//    }
+//
+//    public static void setSpeed(int speed) {
+//        PlayerList.speed = speed;
+//    }
+//
+//    public static int getMaxSpeed() {
+//        return maxSpeed;
+//    }
+//
+//    public static void setMaxSpeed(int maxSpeed) {
+//        PlayerList.maxSpeed = maxSpeed;
+//    }
+//
+//    public static int getLuck() {
+//        return luck;
+//    }
+//
+//    public static void setLuck(int luck) {
+//        PlayerList.luck = luck;
+//    }
+//
+//    public static int getMaxLuck() {
+//        return maxLuck;
+//    }
+//
+//    public static void setMaxLuck(int maxLuck) {
+//        PlayerList.maxLuck = maxLuck;
+//    }
+//
+//    public static int getClassAbility() {
+//        return classAbility;
+//    }
+//
+//    public static void setClassAbility(int classAbility) {
+//        PlayerList.classAbility = classAbility;
+//    }
+//
+//    public static int getLevel() {
+//        return level;
+//    }
+//
+//    public static void setLevel(int level) {
+//        PlayerList.level = level;
+//    }
+//
+//    public static int getXp() {
+//        return xp;
+//    }
+//
+//    public static void setXp(int xp) {
+//        PlayerList.xp = xp;
+//    }
 
     public static String getCompaionName() {
         return compaionName;
@@ -398,33 +398,33 @@ public class PlayerList {
 //
 //
 
-
-    public static String playerName;
-
-    public static String charclass;
-
-    public static int hp;
-    public static int maxHp;
-
-    public static double armor;
-    public static double maxArmor;
-
-    public static int mana;
-    public static int maxMana;
-
-    public static int meleeDmg;
-    public static int maxMeleeDmg;
-
-    public static int speed;
-    public static int maxSpeed;
-
-    public static int luck;
-    public static int maxLuck;
-
-    public static int classAbility;
-
-    public static int level;
-    public static int xp;
+//
+//    public static String playerName;
+//
+//    public static String charclass;
+//
+//    public static int hp;
+//    public static int maxHp;
+//
+//    public static double armor;
+//    public static double maxArmor;
+//
+//    public static int mana;
+//    public static int maxMana;
+//
+//    public static int meleeDmg;
+//    public static int maxMeleeDmg;
+//
+//    public static int speed;
+//    public static int maxSpeed;
+//
+//    public static int luck;
+//    public static int maxLuck;
+//
+//    public static int classAbility;
+//
+//    public static int level;
+//    public static int xp;
 
     //----------------          COMPANIONS
 
@@ -493,122 +493,122 @@ public class PlayerList {
     public static int BossAbility;
 
 
-
-// ---------------      THE KEYS TO THIS BUS
-
-    private static void printStats() {
-        if (charclass.equals("Alchemist")) {
-            System.out.println(playerName + "\nhp: " + hp +
-                    "\nmana: " + mana + "\ndamage: " +
-                    meleeDmg + "\nxp: " + xp + "\n");
-        } else {
-            System.out.println(playerName + "\nhp: " +
-                    hp + "\ndamage: " + meleeDmg +
-                    "\nxp: " + xp + "\n");
-        }
-    }
-
-
-
-    private static void printEnemyStats() {
-        System.out.println("Enemy " + "\nhp: " +
-                enemyHp + "\ndmg: " + enemyMeleeDmg + "\n");
-    }
-
-//--      PC  ADD A FEW MORE CLASSESS
-
-    private static void pathOfTheNomad() {
-        charclass = "Nomad";
-        hp = 20;
-        maxHp = 2000;
-
-        armor = 20;
-        maxArmor = 1000;
-
-        meleeDmg = 4;
-        maxMeleeDmg =
-
-        speed = 12;
-        maxSpeed = 25;
-
-        mana = 15;
-        maxMana = 47;
-
-        luck = 1;
-
-
-        xp = 0;
-        level = 1;
-    }
-
-    private static void pathOfTheAlchemist() {
-        charclass = "Alchemist";
-        hp = 20;
-        maxHp = 2000;
-
-        armor = 20;
-        maxArmor = 1000;
-
-        meleeDmg = 4;
-        maxMeleeDmg =
-
-        speed = 12;
-        maxSpeed = 25;
-
-        mana = 15;
-        maxMana = 47;
-
-        luck = 1;
-
-        xp = 0;
-        level = 1;
-    }
-
-    private static void pathOfTheBerserker() {
-        charclass = "Berserker";
-        hp = 20;
-        maxHp = 2000;
-
-        armor = 20;
-        maxArmor = 1000;
-
-        meleeDmg = 4;
-        maxMeleeDmg =
-
-        speed = 12;
-        maxSpeed = 25;
-
-        mana = 15;
-        maxMana = 47;
-
-        luck = 1;
-
-        xp = 0;
-        level = 1;
-    }
-
-    private static void pathOfThe1() {
-        charclass = "1";
-        hp = 20;
-        maxHp = 2000;
-
-        armor = 20;
-        maxArmor = 1000;
-
-        meleeDmg = 4;
-        maxMeleeDmg =
-
-                speed = 12;
-        maxSpeed = 25;
-
-        mana = 15;
-        maxMana = 47;
-
-        luck = 1;
-
-        xp = 0;
-        level = 1;
-    }
+//
+//// ---------------      THE KEYS TO THIS BUS
+//
+//    private static void printStats() {
+//        if (charclass.equals("Alchemist")) {
+//            System.out.println(playerName + "\nhp: " + hp +
+//                    "\nmana: " + mana + "\ndamage: " +
+//                    meleeDmg + "\nxp: " + xp + "\n");
+//        } else {
+//            System.out.println(playerName + "\nhp: " +
+//                    hp + "\ndamage: " + meleeDmg +
+//                    "\nxp: " + xp + "\n");
+//        }
+//    }
+//
+//
+//
+//    private static void printEnemyStats() {
+//        System.out.println("Enemy " + "\nhp: " +
+//                enemyHp + "\ndmg: " + enemyMeleeDmg + "\n");
+//    }
+//
+////--      PC  ADD A FEW MORE CLASSESS
+//
+//    private static void pathOfTheNomad() {
+//        charclass = "Nomad";
+//        hp = 20;
+//        maxHp = 2000;
+//
+//        armor = 20;
+//        maxArmor = 1000;
+//
+//        meleeDmg = 4;
+//        maxMeleeDmg =
+//
+//        speed = 12;
+//        maxSpeed = 25;
+//
+//        mana = 15;
+//        maxMana = 47;
+//
+//        luck = 1;
+//
+//
+//        xp = 0;
+//        level = 1;
+//    }
+//
+//    private static void pathOfTheAlchemist() {
+//        charclass = "Alchemist";
+//        hp = 20;
+//        maxHp = 2000;
+//
+//        armor = 20;
+//        maxArmor = 1000;
+//
+//        meleeDmg = 4;
+//        maxMeleeDmg =
+//
+//        speed = 12;
+//        maxSpeed = 25;
+//
+//        mana = 15;
+//        maxMana = 47;
+//
+//        luck = 1;
+//
+//        xp = 0;
+//        level = 1;
+//    }
+//
+//    private static void pathOfTheBerserker() {
+//        charclass = "Berserker";
+//        hp = 20;
+//        maxHp = 2000;
+//
+//        armor = 20;
+//        maxArmor = 1000;
+//
+//        meleeDmg = 4;
+//        maxMeleeDmg =
+//
+//        speed = 12;
+//        maxSpeed = 25;
+//
+//        mana = 15;
+//        maxMana = 47;
+//
+//        luck = 1;
+//
+//        xp = 0;
+//        level = 1;
+//    }
+//
+//    private static void pathOfThe1() {
+//        charclass = "1";
+//        hp = 20;
+//        maxHp = 2000;
+//
+//        armor = 20;
+//        maxArmor = 1000;
+//
+//        meleeDmg = 4;
+//        maxMeleeDmg =
+//
+//                speed = 12;
+//        maxSpeed = 25;
+//
+//        mana = 15;
+//        maxMana = 47;
+//
+//        luck = 1;
+//
+//        xp = 0;
+//        level = 1;
+//    }
 
 
     // ----------  NPC
@@ -714,43 +714,43 @@ public class PlayerList {
 //    }
 
 
-    public static void main(String[] args) {
-        String charclass;
-        int num = 2;
-        while (num > 1) {
-            System.out.println("              <--------------->" + "\n" +
-                               "            > Enter your Name, and press" +
-                               "               to start your quest < " + " \n" +
-                               "              <----------------> " );
-            playerName = scan.nextLine();
-            System.out.println("Hello," + playerName + " Choose your class: ");
-            System.out.println(ConsoleColors.WHITE_BOLD  + "'n'--> for Nomad"+ "\n" +
-                    "So " + "\n" + "<------------------------> " +
-                    ConsoleColors.RESET);
-            System.out.println(ConsoleColors.CYAN_BOLD_BRIGHT+ "'a' for Alchemist" + "\n" +
-                    "The elixer is the the real gold,\n" +
-                    "but gold is gold." + "\n" + "<------------------------> " +
-                    ConsoleColors.RESET);
-            System.out.println(ConsoleColors.RED_BOLD + "'b' for Berserker" + "\n" +
-                    "So this might be a good time to run....\n " +
-                    "but if I it's already to late." + "\n" + "<------------------------> " +
-                    ConsoleColors.RESET);
-            charclass = scan.nextLine().toLowerCase();
-
-            if (charclass.charAt(0) == 'n') {
-                pathOfTheNomad();
-            }
-            if (charclass.charAt(0) == 'a') {
-                pathOfTheAlchemist();
-            }
-            if (charclass.charAt(0) == 'b') {
-                pathOfTheBerserker();
-            }
-            printStats();
-
-
-        }
-    }
+//    public static void main(String[] args) {
+//        String charclass;
+//        int num = 2;
+//        while (num > 1) {
+//            System.out.println("              <--------------->" + "\n" +
+//                               "            > Enter your Name, and press" +
+//                               "               to start your quest < " + " \n" +
+//                               "              <----------------> " );
+//            playerName = scan.nextLine();
+//            System.out.println("Hello," + playerName + " Choose your class: ");
+//            System.out.println(ConsoleColors.WHITE_BOLD  + "'n'--> for Nomad"+ "\n" +
+//                    "So " + "\n" + "<------------------------> " +
+//                    ConsoleColors.RESET);
+//            System.out.println(ConsoleColors.CYAN_BOLD_BRIGHT+ "'a' for Alchemist" + "\n" +
+//                    "The elixer is the the real gold,\n" +
+//                    "but gold is gold." + "\n" + "<------------------------> " +
+//                    ConsoleColors.RESET);
+//            System.out.println(ConsoleColors.RED_BOLD + "'b' for Berserker" + "\n" +
+//                    "So this might be a good time to run....\n " +
+//                    "but if I it's already to late." + "\n" + "<------------------------> " +
+//                    ConsoleColors.RESET);
+//            charclass = scan.nextLine().toLowerCase();
+//
+//            if (charclass.charAt(0) == 'n') {
+//                pathOfTheNomad();
+//            }
+//            if (charclass.charAt(0) == 'a') {
+//                pathOfTheAlchemist();
+//            }
+//            if (charclass.charAt(0) == 'b') {
+//                pathOfTheBerserker();
+//            }
+//            printStats();
+//
+//
+//        }
+//    }
 
 }
 
--- src/players.java
+++ src/players.java
@@ -16,12 +16,218 @@ public class players {
     static PlayerList alchmist = new PlayerList();
     static PlayerList berserker = new PlayerList();
 
-    public static void main(String [] args) {
 
+    public players(
+            String playerName, String charclass,
+            int hp, int maxHp,
+            double armor, double maxArmor,
+            int mana, int maxMana,
+            int meleeDmg, int maxMeleeDmg,
+            int speed, int maxSpeed,
+            int luck, int maxLuck,
+            int classAbility, int xp,
+            int level, int maxLevel) {
+
+
+        this.playerName = playerName;
+        this.charclass = charclass;
+        this.hp = hp;
+        this.maxHp = maxHp;
+        this.armor = armor;
+        this.maxArmor = maxArmor;
+        this.mana = mana;
+        this.maxMana = maxMana;
+        this.meleeDmg = meleeDmg;
+        this.maxMeleeDmg = maxMeleeDmg;
+        this.speed = speed;
+        this.maxSpeed = maxSpeed;
+        this.luck = luck;
+        this.maxLuck = maxLuck;
+        this.classAbility = classAbility;
+        this.level = level;
+        this.maxLevel = maxLevel;
+        this.xp = xp;
 
     }
 
+    public static String playerName;
+
+    public static String charclass;
+
+    public static int hp;
+    public static int maxHp;
+
+    public static double armor;
+    public static double maxArmor;
+
+    public static int mana;
+    public static int maxMana;
+
+    public static int meleeDmg;
+    public static int maxMeleeDmg;
+
+    public static int speed;
+    public static int maxSpeed;
+
+    public static int luck;
+    public static int maxLuck;
+
+    public static int classAbility;
+
+    public static int level;
+    public static int maxLevel;
+    public static int xp;
 
 
+// ---------------      THE KEYS TO THIS BUS
 
+    private static void printStats() {
+        if (charclass.equals("Alchemist")) {
+            System.out.println(playerName + "\nhp: " + hp +
+                    "\nmana: " + mana + "\ndamage: " +
+                    meleeDmg + "\nxp: " + xp + "\n");
+        } else {
+            System.out.println(playerName + "\nhp: " +
+                    hp + "\ndamage: " + meleeDmg +
+                    "\nxp: " + xp + "\n");
+        }
+    }
+
+
+//--      PC  ADD A FEW MORE CLASSESS
+
+    private static void pathOfTheNomad() {
+        charclass = "Nomad";
+        hp = 20;
+        maxHp = 2000;
+
+        armor = 20;
+        maxArmor = 1000;
+
+        meleeDmg = 4;
+        maxMeleeDmg =
+
+                speed = 12;
+        maxSpeed = 25;
+
+        mana = 15;
+        maxMana = 47;
+
+        luck = 1;
+
+
+        xp = 0;
+        level = 1;
+    }
+
+    private static void pathOfTheAlchemist() {
+        charclass = "Alchemist";
+        hp = 20;
+        maxHp = 2000;
+
+        armor = 20;
+        maxArmor = 1000;
+
+        meleeDmg = 4;
+        maxMeleeDmg =
+
+                speed = 12;
+        maxSpeed = 25;
+
+        mana = 15;
+        maxMana = 47;
+
+        luck = 1;
+
+        xp = 0;
+        level = 1;
+    }
+
+    private static void pathOfTheBerserker() {
+        charclass = "Berserker";
+        hp = 20;
+        maxHp = 2000;
+
+        armor = 20;
+        maxArmor = 1000;
+
+        meleeDmg = 4;
+        maxMeleeDmg =
+
+                speed = 12;
+        maxSpeed = 25;
+
+        mana = 15;
+        maxMana = 47;
+
+        luck = 1;
+
+        xp = 0;
+        level = 1;
+    }
+
+    private static void pathOfThe1() {
+        charclass = "1";
+        hp = 20;
+        maxHp = 2000;
+
+        armor = 20;
+        maxArmor = 1000;
+
+        meleeDmg = 4;
+        maxMeleeDmg =
+
+                speed = 12;
+        maxSpeed = 25;
+
+        mana = 15;
+        maxMana = 47;
+
+        luck = 1;
+
+        xp = 0;
+        level = 1;
+    }
+
+
+    public static void main(String[] args) {
+
+        String charclass;
+        int num = 2;
+        while (num > 1) {
+            System.out.println("              <--------------->" + "\n" +
+                    "            > Enter your Name, and press" +
+                    "               to start your quest < " + " \n" +
+                    "              <----------------> ");
+            playerName = scan.nextLine();
+            System.out.println("Hello," + playerName + " Choose your class: ");
+            System.out.println(ConsoleColors.WHITE_BOLD + "'n'--> for Nomad" + "\n" +
+                    "So " + "\n" + "<------------------------> " +
+                    ConsoleColors.RESET);
+            System.out.println(ConsoleColors.CYAN_BOLD_BRIGHT + "'a' for Alchemist" + "\n" +
+                    "The elixer is the the real gold,\n" +
+                    "but gold is gold." + "\n" + "<------------------------> " +
+                    ConsoleColors.RESET);
+            System.out.println(ConsoleColors.RED_BOLD + "'b' for Berserker" + "\n" +
+                    "So this might be a good time to run....\n " +
+                    "but if I it's already to late." + "\n" + "<------------------------> " +
+                    ConsoleColors.RESET);
+            charclass = scan.nextLine().toLowerCase();
+
+            if (charclass.charAt(0) == 'n') {
+                pathOfTheNomad();
+            }
+            if (charclass.charAt(0) == 'a') {
+                pathOfTheAlchemist();
+            }
+            if (charclass.charAt(0) == 'b') {
+                pathOfTheBerserker();
+            }
+            printStats();
+
+
+        }
+
+
+    }
 }
