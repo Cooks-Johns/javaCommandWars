626cdffd3c845ba5d1a63415de4e865c6b696a3c
John <cooks.johns@gmail.com>
2018-06-04 20:27:31 -0500

    gitignore

---
+++ src/test/Timer.java
@@ -0,0 +1,43 @@
+package test;
+
+import java.util.Scanner;
+
+public abstract class Timer {
+
+
+    static int i, j;
+
+    public static void hurryUp() {
+        System.out.println("go");
+        System.out.println(" goooo");
+        System.out.println("its going");
+        System.out.println(" its gone");
+
+    }
+
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+        for (j = 0; j >= 0; j++) {
+            for (i = 0; i < 10; i++) {
+                System.out.println(j + ":" + i);
+
+                if (j >= 1) {
+                    hurryUp();
+                } else {
+
+
+                    try {
+                        Thread.sleep(1000);
+
+                    } catch (InterruptedException ie) {
+                        System.out.println(ie.getMessage());
+                    }
+                }
+
+            }
+        }
+    }
+}
