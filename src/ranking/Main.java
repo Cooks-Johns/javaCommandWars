d7b73c5353735db4b581464b7f3f8022ee2c7361
John <cooks.johns@gmail.com>
2018-06-07 00:10:15 -0500

    gn for real this time

--- src/ranking/Main.java
+++ src/ranking/Main.java
@@ -1,19 +1,17 @@
 package ranking;
 
+
 import java.util.Scanner;
 
 public class Main {
+
+
+
     public static void main(String[] args) {
 
 
         Scanner scan = new Scanner(System.in);
 
-
-        // implement your program here
-        // do not put all to one method/class but rather design a proper structure to your program
-
-        // Your program should use only one Scanner object, i.e., it is allowed to call
-        // new Scanner only once. If you need scanner in multiple places, you can pass it as parameter
 
 
         Ranking rank = new Ranking();
@@ -25,6 +23,10 @@ public class Main {
             if(score >= 0 && score <= 60) {
                 rank.add(score);
             }
+
+
+
+            /////---------------> work with moving between files with your code
 
 
         }
