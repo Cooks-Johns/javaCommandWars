5bae36d59b39fc817cedd44c818eb3296290ae78
John <cooks.johns@gmail.com>
2018-08-18 22:59:36 -0400

    factors and made a program that is able to change variables

---
+++ src/ten_java_programs/FactorFriends.java
@@ -0,0 +1,20 @@
+package ten_java_programs;
+
+import java.util.Scanner;
+
+public class FactorFriends {
+
+    public static void main(String[] args) {
+
+        Scanner factor = new Scanner(System.in);
+
+        System.out.println(" Enter the number of the number you want to factor: ");
+        int num1 = factor.nextInt();
+        int flex = factor.nextInt();
+
+        for(int i = num1; i; i--){
+            flex = flex * i;
+
+        }
+    }
+}
---
+++ src/ten_java_programs/NumberSwapNo3rdVariable.java
@@ -0,0 +1,30 @@
+package ten_java_programs;
+
+import java.util.Scanner;
+
+public class NumberSwapNo3rdVariable {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the 1st int: ");
+        int numOne = in.nextInt();
+        System.out.println("Enter the 2nd int: ");
+        int numTwo = in.nextInt();
+
+        System.out.println("Initial value of x:: " +
+                    numOne + " and the second one is " + numTwo);
+
+        numOne = numOne + numTwo;
+        numTwo = numOne - numTwo;
+        numOne = numOne - numTwo;
+
+        System.out.println(
+                "After swapping here are the new values for the 1st and 2nd int "
+                + "(" + numOne + ", " + numTwo + ")");
+
+
+
+    }
+}
